2b67fc82d9412b3b94aaf1663bdbd1902f77902d
Kishan <84682244+Kishan-ui-lab@users.noreply.github.com>
2022-07-16 22:01:43 -0400

    first commit

---
+++ Return.java
@@ -0,0 +1,17 @@
+public class Return {
+	
+	
+	int cable() {
+		
+		return 2;
+
+	}
+
+	public static void main(String[] args) {
+		
+		
+		Return r = new Return();
+		System.out.println(r.cable());
+		
+	}
+}
