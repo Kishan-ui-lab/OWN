952ef9cb5b81dfc485cbf83b8893eefeca8f076e
Kishan <84682244+Kishan-ui-lab@users.noreply.github.com>
2022-07-16 22:07:06 -0400

    2nd commit

---
+++ Practice.java
@@ -0,0 +1,14 @@
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=1;
+		//System.out.println( a++ + a++);  // 
+		//System.out.println( a++); //1
+		System.out.println( ++a);  // 2
+
+	}
+
+}
---
+++ StringConcatenation.java
@@ -0,0 +1,24 @@
+
+public class StringConcatenation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		String x= "Hello";
+		String y="Testing";
+		String s1 ="2";
+		String ph ="5715854665";
+		String cc = "1234 5678 1234";
+		
+		int a = 100;
+		int b = 200;
+		
+		System.out.println(x+y);
+		System.out.println(x+y+a+s1);
+		System.out.println(x+y+a+b);
+		System.out.println(a+b+x);
+		System.out.println(a+x);
+	}
+
+}
